b92045d4ac0b3dac87943f410086f423f5dc8d1b
constantince <alberteinstein007@126.com>
2016-03-22 18:36:50 +0800

    editer the animation code

--- js/slide.js
+++ js/slide.js
@@ -1,6 +1,13 @@
 var slide = (function() {
    //样式命名最好解耦出来，比如按照left或者right或者up和down来命名比较好
-    var animations = ['navOut-left', 'navIn-left', 'navOut-right', 'navIn-right'];
+    var animations = ['navOut-left', 'navIn-left', 'navOut-right', 'navIn-right', 'no-animation-left', 'no-animation-right'];
+    //方向对照表
+    var contraposition = {
+        left: 'right',
+        right: 'left',
+        up: 'down',
+        down: 'up'
+    }
     //去掉浏览器的默认滑动行为
     document.addEventListener('touchmove', function (event) {
        event.preventDefault();
@@ -42,13 +49,29 @@ var slide = (function() {
             brotherRemoveClas: _getOtherCls(brotherAddCls)
         };
     }
-    //负责动画行为
-    function animation (current, direction) {
-        var currentElement = current;
-        var el = _judeElement(currentElement, direction);
+
+    //为相邻的元素添加位置
+    function changePosition(current, direction) {
+        var thisDirection = contraposition[direction];
+        var el = _judeElement(current, thisDirection);
+        el[0].className = 'no-animation-' + thisDirection;
+    }
+
+    //为相邻的元素添加动画
+    function changeAnimation(current, direction) {
+        var el = _judeElement(current, direction);
         var animationObj = _judeClass(direction);
         current.removeClass(animationObj.currentRemoveCls).addClass(animationObj.currentAddCls);
         el.removeClass(animationObj.brotherRemoveClas).addClass(animationObj.brotherAddCls);
+    }
+
+    //负责动画行为
+    function animation (current, direction) {
+        changeAnimation(current, direction);
+        if(current.parent().children().length > 2) {
+            //重置相邻元素的位置
+            changePosition(current, direction);
+        }
     }
 
     return animation;
